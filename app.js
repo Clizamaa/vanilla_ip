128c8683539e795992bc96892b165355ad02876f
Christian <cfla86@gmail.com>
2022-11-27 15:58:06 -0300

    tabla agregada

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ function principal (){
         }
     })
 }
-
 principal();
 
 const select_oficina = async () => {
@@ -140,9 +139,8 @@ const buscarFuncionario = async () => {
     try{
         const response = await fetch('http://localhost:3000/api/funcionario');
         const data = await response.json()
-        // console.log(data)
+        console.log(data)
         for (let i = 0; i < data.length; i++) {
-            // console.log(inputRut.value.length);
             if(inputRut.value.length == 9 && inputRut.value == data[i].rut){
                 nombreFuncionario.value = data[i].nombres;
                 apellidoFuncionario.value = data[i].apellido_pat + " " + data[i].apellido_mat;
@@ -150,12 +148,54 @@ const buscarFuncionario = async () => {
                 nombreFuncionario.value = data[i].nombres;
                 apellidoFuncionario.value = data[i].apellido_pat + " " + data[i].apellido_mat;
             }             
-            
         }
     } catch(error){
         console.log(error);
     }
 }
+
+const todoFuncionario = async () => {
+    try{
+        const response = await fetch('http://localhost:3000/api/funcionario');
+        const data = await response.json()
+
+    } catch(error){
+        console.log(error);
+    }
+}
+new gridjs.Grid({
+    search: true,
+    pagination: {
+        enabled: true,
+        limit: 10,
+    },
+    columns: ['Rut', 'Nombre', 'Apellido_pat', 'Apellido_mat'],
+    server: {
+        url: 'http://localhost:3000/api/funcionario',
+        then: data => data.map(row => ({
+            rut: row.rut,
+            nombre: row.nombres,
+            apellido_pat: row.apellido_pat,
+            apellido_mat: row.apellido_mat,
+        }))
+    },
+
+    style: {
+        table: {
+          border: '3px solid #ccc',
+          width: '100%'
+        },
+        th: {
+          'background-color': 'rgba(0, 0, 0, 0.1)',
+          color: '#000',
+          'border-bottom': '3px solid #ccc',
+          'text-align': 'center'
+        },
+        td: {
+          'text-align': 'center'
+        }
+      }
+  }).render(document.getElementById("wrapper"))
 
 
 
