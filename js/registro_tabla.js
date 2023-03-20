6e1a377a9edc131938c76d1e4e9ba484ca85eac8
Christian <cfla86@gmail.com>
2023-03-20 16:43:52 -0300

    tabla2

--- js/registro_tabla.js
+++ js/registro_tabla.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', async()=>{
+  await initDataTable();
+ });
+ 
 //configuracion de la tabla
 let dataTable;
 let dataTableIsInitialized = false;
@@ -8,7 +12,33 @@ const initDataTable = async() => {
     }
 
     await listUser();
-    dataTable = $("#datatable_users").dataTable({});
+    dataTable = $("#datatable_users").dataTable({
+      lengthMenu: [
+        [5, 10, 15, -1],
+        [5, 10, 15, "All"]
+      ],
+      language: {
+        search: "_INPUT_",
+        searchPlaceholder: "Buscar...",
+        lengthMenu: "Mostrar _MENU_ registros",
+        zeroRecords: "No se encontraron resultados",
+        info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+        infoFiltered: "(filtrado de un total de _MAX_ registros)",
+        paginate: {
+          first: "Primero",
+          last: "Último",
+          next: "Siguiente",
+          previous: "Anterior"
+        },
+        aria: {
+          sortAscending: ": Activar para ordenar la columna de manera ascendente",
+          sortDescending: ": Activar para ordenar la columna de manera descendente"
+        }
+      },
+      responsive: true,
+      autoWidth: false,
+    });
     dataTableIsInitialized = true;
 };
 
@@ -26,6 +56,7 @@ const listUser= async()=>{
                     <td>${user.rut}</td>
                     <td>${user.nombres}</td>
                     <td>${user.apellido_pat +" "+ user.apellido_mat}</td>
+                    <td>${user.id_listadoIpes}</td>
                     <td>
                     <button type="button" class="btn btn-warning"><i class="fa-solid fa-pencil"></i></button> 
                     <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
@@ -39,9 +70,8 @@ const listUser= async()=>{
     }
 };
 
-window.addEventListener('load', async()=>{
- await initDataTable();
-});
+//funcion editar un usuario
+
 
 
 
